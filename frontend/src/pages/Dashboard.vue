613062f0f2f73dd3f24e2d6575a16aa1a613baa4
rita.radovics <rita.radovics@agillic.com>
2019-05-12 01:16:19 +0200

    getallfriends and getonefriend functions

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,15 +1,65 @@
 <template>
   <div v-if="$root.user" class="dashboard">
-    DASHBOARD
+    <div @click="goToFriend(friend)" v-for="friend in $root.friends" class="friendContainer">
+      <img :src="friend.image_url">
+      <div class="name">{{friend.first_name}} {{friend.last_name}}</div>
+      <div>blahjgjyfkgvhj</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'dashboard'
+  created(){
+    this.getAllFriends()
+  },
+  methods:{
+    getAllFriends(){
+      fetch('/friends/backend/api/api-get-all-friends.php', {
+        method: 'GET',
+        credentials: 'include'
+      })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        //check if its 1
+        this.$root.friends = json.data
+      }).catch(error => {
+
+        console.log(error)
+      })
+    },
+    goToFriend(friend){
+      console.log(friend)
+      this.$router.push(`friend/${friend.id}`);
+    }
+  }
 }
 </script>
 
 <style lang="stylus">
+.dashboard
+  display flex
+  flex-direction column
+  padding-top 80px
+  width 700px
+
+.friendContainer
+  display flex
+  flex-direction row
+  align-items center
+  padding-bottom 20px
+  padding-left 70px
+  cursor pointer
+
+  img
+    width 80px
+    height 80px
+    object-fit cover
+    border-radius 50%
+    margin-right 40px
+
+  .name
+    margin-right 40px
 
 </style>
