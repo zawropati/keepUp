bb7a722b373c91b4af1717ea2d528f9de4e7ca83
rita.radovics <rita.radovics@agillic.com>
2019-05-12 18:11:36 +0200

    dignin signup styled

--- frontend/src/pages/Signin.vue
+++ frontend/src/pages/Signin.vue
@@ -2,7 +2,7 @@
   <div class="signin">
     <input type="text" id="email" v-model="email" placeholder="Email">
     <input type="password" id="userPassword" v-model="userPassword" placeholder="Password">
-    <button @click="signInUser">Sign in</button>
+    <button @click="signInUser">Sign in <img class="arrow" src='../assets/arrow-right-solid.svg'></button>
   </div>
 </template>
 
@@ -39,5 +39,42 @@ export default {
 </script>
 
 <style lang="stylus">
+.signin
+  display flex
+  flex-direction column
+  padding-top 150px
+
+  input
+    padding 15px
+    width 300px
+    margin-bottom 30px
+    border-radius 25px
+    border 3px white solid
+    background-color white
+    outline none
+    box-shadow 3px 6px 19px -10px #ccc
+    color #565051
+
+  button
+    padding 15px
+    margin-bottom 20px
+    // border-radius 25px
+    background-color transparent
+    border none
+    color #565051
+    font-weight bold
+    width 50%
+    align-self center
+    cursor pointer
+
+    .arrow
+      width 20px
+      position relative
+      top 8px
+      left 3px
+      color white
+
+::placeholder
+  color #565051
 
 </style>
--- frontend/src/pages/Signup.vue
+++ frontend/src/pages/Signup.vue
@@ -4,10 +4,10 @@
     <input type="text" id="lastName" v-model="lastName" placeholder="Last name">
     <input type="text" id="email" v-model="email" placeholder="Email">
     <input type="password" id="userPassword" v-model="userPassword" placeholder="Password">
-    <button @click="signUpUser">Sign up</button>
+    <button @click="signUpUser">Sign up <img class="arrow" src='../assets/arrow-right-solid.svg'> </button>
   </div>
   <div v-else>
-    <p>One step closer to nurture your relationships.</p>
+    <p>One step closer to nurture your relationships. You can find the Log in button in the right upper corner.</p>
   </div>
 </template>
 
@@ -52,24 +52,39 @@ export default {
 .signup
   display flex
   flex-direction column
-  padding-top 80px
+  padding-top 100px
 
   input
-    padding 10px
+    padding 15px
     width 300px
-    margin-bottom 20px
+    margin-bottom 30px
     border-radius 25px
     border 3px white solid
-    background transparent
+    background-color white
     outline none
+    box-shadow 3px 6px 19px -10px #ccc
+    color #565051
 
   button
-    padding 10px
+    padding 15px
     margin-bottom 20px
-    border-radius 25px
-    background-color brandGreen
+    // border-radius 25px
+    background-color transparent
     border none
-    color white
+    color #565051
     font-weight bold
+    width 50%
+    align-self center
+    cursor pointer
+
+    .arrow
+      width 20px
+      position relative
+      top 8px
+      left 3px
+      color white
+
+::placeholder
+  color #565051
 
 </style>
