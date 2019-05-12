e44a0e0a635a8ee1f2da367838ea4366526c064a
rita.radovics <rita.radovics@agillic.com>
2019-05-13 00:54:04 +0200

    get all friends also gets friend category

--- backend/api/api-get-all-friends.php
+++ backend/api/api-get-all-friends.php
@@ -10,7 +10,7 @@ if( !isset ($_SESSION['userID'])){
 $userID = $_SESSION['userID'];
 
 
-$stmt = $db->prepare('SELECT id, first_name, last_name, image_url FROM friends WHERE user_fk=:userID');
+$stmt = $db->prepare('SELECT id, first_name, last_name, image_url, category_fk FROM friends WHERE user_fk=:userID');
 $stmt->bindValue(':userID', $userID);
 $stmt->execute();
 $friends = $stmt->fetchAll();
